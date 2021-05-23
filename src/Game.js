013127a11240be7cf196f83c770ca9cbbaeccb15
Alexander-Ryzhkov <alexander.ryzhkov.96@gmail.com>
2021-05-23 21:51:38 +0300

    reorg pick cards for bot

--- src/Game.js
+++ src/Game.js
@@ -124,14 +124,12 @@ export const BoardGame = {
                     if (G.taverns[tavernId][i] === null) {
                         continue;
                     }
-                    if (G.taverns[tavernId].some(element => CompareCards(G.taverns[tavernId][i], element) === -1)) {
+                    if (G.taverns[tavernId].some(element => CompareCards(G.taverns[tavernId][i], element) < 0)) {
                         continue;
                     }
-                    if (G.decks[0].length > 18) {
-                        const curSuit = G.taverns[tavernId][i].suit;
-                        if ((CompareCards(G.taverns[tavernId][i], G.averageCards[curSuit]) === -1) && G.taverns[tavernId].some(element => (element !== null) && (CompareCards(element, G.averageCards[curSuit]) > -1))) {
-                            continue;
-                        }
+                    const curSuit = G.taverns[tavernId][i].suit;
+                    if ((CompareCards(G.taverns[tavernId][i], G.averageCards[curSuit]) < 0) && G.taverns[tavernId].some(element => (element !== null) && (CompareCards(element, G.averageCards[curSuit]) >= 0))) {
+                        continue;
                     }
                     const uniqueArrLength = uniqueArr.length;
                     for (let j = 0; j < uniqueArrLength; j++) {
@@ -164,36 +162,6 @@ export const BoardGame = {
             }
             if (enableAdvancedBot && ctx.phase === 'placeCoins') {
                 moves = [];
-                // const marketCoinsMaxValue = G.marketCoins.reduce((prev, current) => (prev.value > current.value) ? prev.value : current.value, 0);
-                // let potentialScores = [];
-                /*let curCards = [];
-                for (let i = 0; i < G.players[ctx.currentPlayer].cards.length; i++) {
-                    curCards[i] = [];
-                    for (let j = 0; j < G.players[ctx.currentPlayer].cards[i].length; j++) {
-                        AddCardToCards(curCards, G.players[ctx.currentPlayer].cards[i][j]);
-                    }
-                }*/
-                // let curScore = PotentialScoring({cards: curCards});
-                /*const indifferentTaverns = Array(G.tavernsNum).fill(true);
-                const temp = [];
-                for (let i = 0; i < G.tavernsNum; i++) {
-                    for (let j = 0; j < G.taverns[i].length; j++) {
-                            temp[j] = CompareCards(G.taverns[i][j], G.averageCards[G.taverns[i][j].suit]);
-                            if ((j > 0) && (temp[j] !== temp[j - 1])) {
-                                indifferentTaverns[i] = false;
-                                break;
-                            }
-                    }
-                }*/
-                const res = [];
-                for (let i = 0; i < G.botData.allPicks.length; i++) {
-                    let temp = 0;
-                    for (let j = 0; j < G.tavernsNum; j++) {
-                        temp += CompareCards(G.taverns[j][G.botData.allPicks[i][j]], G.averageCards[G.taverns[j][G.botData.allPicks[i][j]].suit]);
-                    }
-                    res.push(temp);
-                }
-
                 const hasLowestPriority = HasLowestPriority(G.players, ctx.currentPlayer);
                 let resultsForCoins = CheckHeuristicsForCoinsPlacement(G.taverns, G.averageCards);
                 if (hasLowestPriority) {
@@ -201,7 +169,7 @@ export const BoardGame = {
                 }
                 const minResultForCoins = Math.min(...resultsForCoins),
                     maxResultForCoins = Math.max(...resultsForCoins),
-                    tradingProfit = G.decks[G.decks.length - 1].length > 18 ? 1: 0;
+                    tradingProfit = G.decks[G.decks.length - 1].length > 9 ? 1 : 0;
                 let [positionForMinCoin, positionForMaxCoin] = [-1, -1];
                 if (minResultForCoins <= 0) {
                     positionForMinCoin = resultsForCoins.findIndex(item => item === minResultForCoins);
